47ec75ccc7179f5c39870b0dfcda8e7379011713
AnshB0712 <anshulbhorde421@gmail.com>
2022-09-17 15:17:51 +0530

    posts initial state changed

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,33 +1,11 @@
 import { createSlice,nanoid } from '@reduxjs/toolkit'
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: new Date().toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: new Date().toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    }
-]
+const initialState = {
+    posts: [],
+    status: 'idle',
+    error: null
+}
+
 
 export const postsSlice = createSlice
 ({
